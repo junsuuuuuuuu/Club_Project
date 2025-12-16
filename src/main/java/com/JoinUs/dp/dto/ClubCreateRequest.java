a77218f0a6b0c5cd17dd2879c5d9d08f762b1884
Junsu <junsu0403@naver.com>
2025-12-16 23:54:30 +0900

    feat:수정

--- src/main/java/com/JoinUs/dp/dto/ClubCreateRequest.java
+++ src/main/java/com/JoinUs/dp/dto/ClubCreateRequest.java
@@ -3,14 +3,22 @@ package com.JoinUs.dp.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class ClubCreateRequest {
     private String name;
-    private String shortDesc;
+    private String shortDescription;
     private String description;
-    private String type;       // general / major
-    private String department;
+    private String type;               // general / major
     private String category;
+    private String department;
     private Long leaderId;
+
+    private Boolean isRecruiting;      // 모집 상태
+    private String recruitDeadline;    // yyyy-MM-dd
+    private Integer members;           // 초기 인원
+    private List<String> activities;   // 활동 목록
+    private String direction;          // 비전/방향
 }
